7d917f8f37a5e55bc473af38e438b235e2e3f28d
yeokyoung song <spolice0324@gmail.com>
2024-02-03 00:11:49 +0900

    fix: conflict 해결

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import { Button } from '@/components/button'
-import { signIn } from 'next-auth/react'
-=======
 import Button from '@/components/button'
 import { signIn } from '@/lib/client/auth'
->>>>>>> origin
 import React from 'react'
 
 const Signin = () => {
