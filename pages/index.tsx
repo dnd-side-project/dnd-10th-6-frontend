ecc0ca6d6ff62c0150c7bfb6cbfa865727ebfc78
yeokyoung song <spolice0324@gmail.com>
2024-02-13 23:53:26 +0900

    fix : 병합 충돌 해결

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,8 @@
-<<<<<<< HEAD
-import Button from '@/components/button'
-import Carousel from '@/components/carousel'
-import Inputbox from '@/components/inputbox'
-import { media } from '@/components/media'
-import { useSession } from '@/provider/session-provider'
-import { Modal } from '@/components/modal'
-
-import Image from 'next/image'
-import React from 'react'
-=======
 import { GetServerSideProps } from 'next'
 
->>>>>>> 1c5d4b42b14c7562006a04157c89769a340a9aa7
 const Page = () => {
   return (
-<<<<<<< HEAD
-    <div className="p-4">
-      <Inputbox />
-      <Carousel
-        slides={media}
-        renderItem={(item, index) => (
-          <Image
-            key={index}
-            height={300}
-            width={600}
-            src={item.src}
-            placeholder="blur"
-            blurDataURL={item.blurDataURL}
-            alt={item.src}
-          />
-        )}
-      />
-
-      <Button
-        disabled={status === 'loading' || !!data}
-        onClick={() => signin({ provider: 'kakao' })}
-      >
-        {data ? '접속중' : 'Signin'}
-      </Button>
-      <Button onClick={() => signout()}>Signout</Button>
-
-      <Modal
-        title="Modal Title"
-        description="Modal Description"
-        trigger={<Button>Open Modal</Button>}
-      >
-        Modal Content
-      </Modal>
-    </div>
-=======
     <div className="min-h-[100dvh] flex flex-col pb-[50px] px-5">INDEX</div>
->>>>>>> 1c5d4b42b14c7562006a04157c89769a340a9aa7
   )
 }
 
