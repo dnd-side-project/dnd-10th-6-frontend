69595bb2f7d41cc401f25e058cead30722d5aac6
yeokyoung song <spolice0324@gmail.com>
2024-02-19 00:59:24 +0900

    feat: combobox 컴포넌트 수정

--- components/combobox/index.tsx
+++ components/combobox/index.tsx
@@ -1,124 +1,99 @@
 import { useState } from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
 import { cn } from '@/lib/client/utils'
+import { forwardRef } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+
 const variants = {
-  open: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      staggerChildren: 0.05,
-      type: 'spring',
-      stiffness: 500,
-      damping: 30,
-    },
-  },
-  closed: {
-    opacity: 0,
-    y: -10,
-    transition: {
-      staggerChildren: 0.05,
-      type: 'spring',
-      stiffness: 500,
-      damping: 30,
-    },
-  },
+  initial: { opacity: 0, y: -10 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: -10 },
 }
 
-interface ComboboxDropdownProps {
-  options: { label: string; value: string }[]
-  onSelect: (selectedOption: string) => void
+interface Option {
+  label: string
+  value: string
+}
+
+interface CombocoxDropdownProps {
+  options: Option[]
+  placeholder: string
+  name: string
   disabled?: boolean
-  className?: string
-  placeholder?: string
+  onChange: (value: string) => void
 }
 
-const ComboboxDropdown: React.FC<ComboboxDropdownProps> = ({
-  options,
-  onSelect,
-  disabled = false,
-  className,
-  placeholder,
-}) => {
-  const [isOpen, setIsOpen] = useState(false)
-  const [selectedOption, setSelectedOption] = useState<string | null>(null)
+const ComboboxDropdown = forwardRef<HTMLDivElement, CombocoxDropdownProps>(
+  ({ options, placeholder, disabled, onChange }, ref) => {
+    const [isOpen, setIsOpen] = useState(false)
+    const [selectedOption, setSelectedOption] = useState<Option | null>(null)
 
-  const handleOptionClick = (value: string) => {
-    setSelectedOption(value)
-    onSelect(value)
-    setIsOpen(false)
-  }
+    const handleOptionClick = (option: Option) => {
+      setSelectedOption(option)
+      setIsOpen(false)
+      onChange(option.value)
+    }
 
-  return (
-    <div className={cn('relative', className)}>
-      <button
-        onClick={() => setIsOpen(!isOpen)}
-        className={cn(
-          'flex items-center justify-between w-full px-4 py-2 text-left bg-white border border-[#E5E5EC] rounded-md shadow-sm ',
-          'focus:outline-none focus:border-brand-main-green400',
-          disabled && 'text-text-sub-gray99 bg-gray-gray50  cursor-not-allowed',
-          !disabled && 'hover:bg-gray-gray50',
-        )}
-        disabled={disabled}
-      >
-        <span className={cn(selectedOption && 'font-bold')}>
-          {selectedOption ? selectedOption : placeholder}
-        </span>
-
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-5 h-5 text-text-sub-gray76"
+    return (
+      <div ref={ref} className="relative" onBlur={() => setIsOpen(false)}>
+        <div
+          className={cn(
+            'flex items-center justify-between w-full px-4 py-2 text-left bg-white border border-[#E5E5EC] rounded-md shadow-sm ',
+            'focus:outline-none focus:border-brand-main-green400 ',
+            disabled &&
+              'text-text-sub-gray99 bg-gray-gray50  cursor-not-allowed',
+            !disabled && 'hover:bg-gray-gray50',
+          )}
+          onClick={() => setIsOpen(!isOpen)}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="m19.5 8.25-7.5 7.5-7.5-7.5"
-          />
-        </svg>
-      </button>
-      <AnimatePresence>
-        {isOpen && (
-          <motion.ul
-            key="options"
-            initial="closed"
-            animate="open"
-            exit="closed"
-            variants={variants}
-            className="absolute z-10 w-full mt-2 bg-white rounded-md shadow-lg"
+          <span className={cn(selectedOption && 'font-bold')}>
+            {selectedOption ? selectedOption.label : placeholder}
+          </span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5 text-text-sub-gray76"
           >
-            {options.map((option, index) => {
-              return (
-                <motion.li
-                  key={index}
-                  onClick={() => handleOptionClick(option.value)}
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m19.5 8.25-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </div>
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div
+              className="absolute z-10 mt-1 w-full bg-white border border-[#E5E5EC] rounded-md shadow-lg"
+              variants={variants}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+            >
+              {options.map((option, index) => (
+                <motion.div
+                  key={option.value}
                   className={cn(
                     'px-4 py-2 cursor-pointer select-none text-left',
                     index === options.length - 1 ? 'rounded-b-md' : '',
-                    selectedOption === option.value && 'font-bold',
+                    selectedOption?.value === option.value && 'font-bold',
                     !disabled && 'hover:bg-gray-gray50',
                   )}
-                  initial={{ opacity: 0, y: -10 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{
-                    delay: index * 0.07,
-                    duration: 0.3,
-                    ease: 'easeInOut',
-                    type: 'spring',
-                    stiffness: 500,
-                    damping: 20,
-                  }}
+                  onClick={() => handleOptionClick(option)}
+                  variants={variants}
                 >
                   {option.label}
-                </motion.li>
-              )
-            })}
-          </motion.ul>
-        )}
-      </AnimatePresence>
-    </div>
-  )
-}
+                </motion.div>
+              ))}
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+    )
+  },
+)
+
+ComboboxDropdown.displayName = 'ComboboxDropdown'
 export default ComboboxDropdown
