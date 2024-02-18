752bc7e709da4f4bb63282f69c3d10d089649488
yeokyoung song <spolice0324@gmail.com>
2024-02-19 02:35:07 +0900

    feat: zod 추가

--- components/form-contents/InputName.tsx
+++ components/form-contents/InputName.tsx
@@ -8,9 +8,13 @@ import { FormValues } from '@/pages/surveys/hooks/useSurveyForm'
 const InputName = () => {
   const { toNextStep } = useFunnelContext()
 
-  const { setValue, control } = useFormContext<FormValues>()
-  const { name } = useWatch({ control })
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext<FormValues>()
 
+  const { name } = useWatch({ control })
+  const isNameValid = !errors.name && name
   return (
     <div className="min-h-[100dvh] flex flex-col pb-[50px] px-5">
       <div className="flex-1 flex flex-col mt-4 ">
@@ -22,17 +26,21 @@ const InputName = () => {
               <Inputbox
                 {...field}
                 placeholder="이름을 입력해주세요"
-                onChange={(e) => {
-                  setValue('name', e.target.value)
-                }}
+                maxLength={6}
               />
             )}
           />
         </InputLabel>
+        {errors.name && (
+          <span className="text-red-500">{errors.name.message}</span>
+        )}
+        <p className="ml-2 mt-2 text-body3-medium text-text-sub-gray76">
+          2-6자로 입력해주세요
+        </p>
       </div>
 
       <div className="fixed bottom-0 left-0 right-0 p-5 mb-4 bg-white flex justify-center">
-        <Button onClick={toNextStep} disabled={!name}>
+        <Button onClick={toNextStep} disabled={!isNameValid}>
           다음
         </Button>
       </div>
--- pages/surveys/hooks/useSurveyForm.ts
+++ pages/surveys/hooks/useSurveyForm.ts
@@ -1,10 +1,14 @@
 import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
-export interface FormValues {
-  name: string
-  knowingRoute: string
-  knowingPeriod: string
-}
+const FormSchema = z.object({
+  name: z.string().min(2).max(6),
+  knowingRoute: z.string(),
+  knowingPeriod: z.string(),
+})
+
+export type FormValues = z.infer<typeof FormSchema>
 
 const useSurveyForm = () => {
   return useForm<FormValues>({
@@ -13,6 +17,7 @@ const useSurveyForm = () => {
       knowingRoute: '',
       knowingPeriod: '',
     },
+    resolver: zodResolver(FormSchema),
   })
 }
 
