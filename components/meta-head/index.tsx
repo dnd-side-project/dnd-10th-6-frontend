f0cc10fd6919eb545c57e4960d2621f7b02e2166
Raymond <akdfhr2@gmail.com>
2024-02-22 04:12:07 +0900

    fix: 프로퍼티명 변경

--- components/meta-head/index.tsx
+++ components/meta-head/index.tsx
@@ -20,7 +20,7 @@ const MetaHead = ({
       <meta property="og:title" content={title || 'namuiwiki | 남의위키'} />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={url || 'https://namui-wiki.life/'} />
-      <meta name="og:description" content={description || ''} />
+      <meta property="og:description" content={description || ''} />
 
       <meta
         property="og:image"
