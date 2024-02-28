63a666a50324678dc6248f09b7530452c100a121
Raymond <akdfhr2@gmail.com>
2024-02-28 13:19:34 +0900

    fix: 모바일 Drawer 애니메이션 변경

--- components/ui/drawer/index.tsx
+++ components/ui/drawer/index.tsx
@@ -1,6 +1,6 @@
 import { HeaderProps } from '@/components/header'
 import BaseLayout from '@/layout/base-layout'
-import { useBrowserLayoutEffect } from '@/lib/client/utils'
+import { isMobile, useBrowserLayoutEffect } from '@/lib/client/utils'
 import { drawerInOutProps } from '@/variants'
 import { AnimatePresence, LazyMotion, domAnimation } from 'framer-motion'
 import {
@@ -105,20 +105,37 @@ const Drawer = ({
       {ref.current
         ? createPortal(
             <LazyMotion features={domAnimation}>
-              <AnimatePresence mode="wait">
-                {isMounted ? (
-                  <BaseLayout
-                    className="w-screen bg-white h-calc-h"
-                    framer={{ ...drawerInOutProps }}
-                    showHeader={header?.showHeader}
-                    header={header}
-                  >
-                    <section className="flex flex-col grow overflow-y-scroll h-full">
-                      {children}
-                    </section>
-                  </BaseLayout>
-                ) : null}
-              </AnimatePresence>
+              {isMobile() ? (
+                <>
+                  {isMounted ? (
+                    <BaseLayout
+                      className="w-screen bg-white h-calc-h"
+                      framer={drawerInOutProps}
+                      showHeader={header?.showHeader}
+                      header={header}
+                    >
+                      <section className="flex flex-col grow overflow-y-scroll h-full">
+                        {children}
+                      </section>
+                    </BaseLayout>
+                  ) : null}
+                </>
+              ) : (
+                <AnimatePresence presenceAffectsLayout>
+                  {isMounted ? (
+                    <BaseLayout
+                      className="w-screen bg-white h-calc-h"
+                      framer={{ ...drawerInOutProps }}
+                      showHeader={header?.showHeader}
+                      header={header}
+                    >
+                      <section className="flex flex-col grow overflow-y-scroll h-full">
+                        {children}
+                      </section>
+                    </BaseLayout>
+                  ) : null}
+                </AnimatePresence>
+              )}
             </LazyMotion>,
             ref.current,
           )
