36dac280c332e0cc9151ff787a8f378f463d44ae
Raymond <akdfhr2@gmail.com>
2024-03-05 23:36:50 +0900

    feat: 내가 쓴 목록 보기

--- components/compositions/header/write-list/index.tsx
+++ components/compositions/header/write-list/index.tsx
@@ -1,9 +1,13 @@
 import Drawer from '@/components/ui/drawer'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useState } from 'react'
 import WriteListCard from '@/components/compositions/header/write-list/wirte-list-card'
 import useFilter, { Filter } from '@/hooks/use-filter'
 import { motion } from 'framer-motion'
 import { useIntersectionObserver } from '@/hooks/use-observer'
+import { InfiniteData, useInfiniteQuery } from '@tanstack/react-query'
+import { WritingListResponse } from '@/queries/surveys'
+import { NamuiApi } from '@/lib/namui-api'
+
 const container = {
   hidden: { opacity: 0 },
   show: {
@@ -16,11 +20,33 @@ const container = {
 
 const WriteList = () => {
   const { selectedFilter } = useFilter()
+  const { data, isLoading, fetchNextPage, hasNextPage } = useInfiniteQuery<
+    WritingListResponse,
+    Error,
+    InfiniteData<WritingListResponse>,
+    [string, string, string]
+  >({
+    initialPageParam: 0,
+    getNextPageParam: (page) => {
+      return page.data.totalPage <= page.data.page
+        ? undefined
+        : page.data.page + 1
+    },
+    queryKey: [
+      'my-writing-list',
+      selectedFilter?.type ?? 'period',
+      selectedFilter?.value ?? 'ALL',
+    ],
+    queryFn: ({ pageParam = 0, queryKey }) =>
+      NamuiApi.getMyWritingList(pageParam as number, [
+        queryKey[1],
+        queryKey[2],
+      ]),
+  })
   const [openAlert, setOpenAlert] = useState(false)
-  const [length, setLength] = useState(10)
   const { ref } = useIntersectionObserver<HTMLDivElement>({
-    hasNextPage: true,
-    fetchNextPage: () => setLength((prev) => prev + 10),
+    hasNextPage,
+    fetchNextPage,
   })
 
   return (
@@ -43,16 +69,48 @@ const WriteList = () => {
       }
     >
       <Filter />
-      <div className="p-5 flex flex-col">
-        <p className="text-body3-medium text-text-sub-gray76 mb-4">
-          <b className="text-brand-main-green400">999명</b>의 친구가 이유를
-          적었어요
+      <div className="px-5">
+        <p className="text-body3-medium text-text-sub-gray76 mt-1 mb-4 flex items-center">
+          {typeof data?.pages[0].data.totalCount !== 'number' ? (
+            <b className="text-brand-main-green400 skeleton w-10 h-4 block mr-1"></b>
+          ) : (
+            <b className="text-brand-main-green400">
+              {data?.pages[0].data.totalCount}명
+            </b>
+          )}
+          의 남의위키를 작성했어요
         </p>
 
         <motion.ul initial="hidden" animate="show" variants={container}>
-          {Array.from({ length }, (v, i) => i + 1).map((item) => (
-            <WriteListCard key={item} />
-          ))}
+          {!isLoading && data
+            ? data.pages.map((page, pageNo) =>
+                page.data.content.map((item, index) => (
+                  <WriteListCard key={index + item.surveyId} item={item} />
+                )),
+              )
+            : Array.from({ length: 10 }).map((_, index) => (
+                <motion.div
+                  key={`card-skeleton-${index}`}
+                  variants={variants}
+                  className="flex py-5 justify-between space-x-4"
+                >
+                  <div className="aspect-square rounded-full skeleton h-12  flex justify-center items-center"></div>
+                  <div className="flex flex-col grow space-y-4">
+                    <div className="flex flex-col space-y-1">
+                      <h3 className="text-body1-bold skeleton h-4 w-1/5"></h3>
+                      {/* <p className="text-body3-medium text-text-sub-gray76 skeleton h-4 w-1/2"></p> */}
+                    </div>
+                    {/* <div>{cardItem.answer}</div> 뱃지들어가야함 */}
+                    <div className="flex">
+                      <div className="flex h-4 grow gap-x-2">
+                        <p className="text-body1-medium text-text-main-black11 skeleton w-1/4 h-4"></p>
+                        <p className="text-body1-medium text-text-main-black11 skeleton w-1/4 h-4"></p>
+                      </div>
+                      <p className="text-body1-medium text-text-main-black11 skeleton w-2/5 h-4"></p>
+                    </div>
+                  </div>
+                </motion.div>
+              ))}
         </motion.ul>
         <div ref={ref} className="h-2 w-2" />
       </div>
@@ -61,3 +119,15 @@ const WriteList = () => {
 }
 
 export default WriteList
+const variants = {
+  hidden: {
+    opacity: 0,
+    y: 10,
+    transition: { ease: [0.78, 0.14, 0.15, 0.86] },
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: { ease: [0.78, 0.14, 0.15, 0.86] },
+  },
+}
