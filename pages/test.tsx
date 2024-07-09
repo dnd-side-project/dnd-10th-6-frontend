b46fbdf3b822b3dc3babccb3393fc7e29cc94409
yeokyoung song <spolice0324@gmail.com>
2024-07-09 23:50:43 +0900

    feat: 인풋 variants 수정

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,10 +1,32 @@
 import React, { ReactNode } from 'react'
-import { Inputbox } from '@/components/ui'
+import { ComboboxDropdown, Inputbox } from '@/components/ui'
 
 const Pages = () => {
   return (
-    <div className="flex h-screen items-center px-3">
-      <Inputbox placeholder="디폴트" />
+    <div className="flex h-screen w-full items-center  px-3">
+      <ComboboxDropdown
+        placeholder="알게 된 기간을 선택해주세요"
+        options={[
+          {
+            label: '6개월미만',
+            value: 'six_months'.toUpperCase(),
+          },
+          {
+            label: '6개월 - 1년미만',
+            value: 'one_year'.toUpperCase(),
+          },
+          {
+            label: '1년 - 4년미만',
+            value: 'four_years'.toUpperCase(),
+          },
+          {
+            label: '4년이상',
+            value: 'infinite'.toUpperCase(),
+          },
+        ]}
+      />
+
+      <Inputbox type="text" placeholder="알게 된 기간을 선택해주세요" />
     </div>
   )
 }
