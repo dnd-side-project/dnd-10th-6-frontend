5b7e825c9fc39dcec255f023d16e524227083fe1
yeokyoung song <spolice0324@gmail.com>
2024-08-28 00:34:05 +0900

    feat: welcome 수정

--- pages/welcome/index.tsx
+++ pages/welcome/index.tsx
@@ -2,9 +2,6 @@ import React, { useCallback, useMemo, useState } from 'react'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
 import { useSession } from '@/provider/session-provider'
-
-import FormLayout from '@/layout/form-layout'
-
 import MetaHead from '@/components/meta-head'
 import Modal from '@/components/modal'
 import { Button, Drawer, DrawerContent, DrawerFooter } from '@/components/ui'
@@ -15,6 +12,7 @@ import { PropswithWikiType, WikiType, wikiTypeList } from '@/types'
 import ShareModal from '@/components/share-modal'
 import { Controller, useForm } from 'react-hook-form'
 import { cn } from '@/lib/client/utils'
+import BaseLayout from '@/layout/base-layout'
 
 const WelcomePage = ({ wikiType }: PropswithWikiType) => {
   const router = useRouter()
@@ -47,74 +45,52 @@ const WelcomePage = ({ wikiType }: PropswithWikiType) => {
         description="남의위키를 통해 남이 써준 나의 소개서를 확인해보세요!"
         url="https://namui-wiki.life/welcome"
       />
-      <FormLayout
-        header={{
-          leftIcon: <></>,
-          center: <></>,
-          rightIcon: (
-            <button
-              onClick={() => {
-                router.replace('/main')
-              }}
-            >
-              <svg
-                width="28"
-                height="28"
-                viewBox="0 0 28 28"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M22.786 6.45173C23.1277 6.11002 23.1277 5.556 22.786 5.21429C22.4443 4.87258 21.8903 4.87258 21.5486 5.21429L14.0007 12.7622L6.45271 5.21429C6.111 4.87258 5.55698 4.87258 5.21527 5.21429C4.87356 5.556 4.87356 6.11002 5.21527 6.45173L12.7632 13.9997L5.21527 21.5476C4.87356 21.8893 4.87356 22.4434 5.21527 22.7851C5.55697 23.1268 6.11099 23.1268 6.4527 22.7851L14.0007 15.2371L21.5486 22.7851C21.8903 23.1268 22.4443 23.1268 22.786 22.7851C23.1277 22.4434 23.1277 21.8893 22.786 21.5476L15.2381 13.9997L22.786 6.45173Z"
-                  fill="#111111"
-                />
-              </svg>
-            </button>
-          ),
-        }}
-        className="fixed left-0 top-0 z-10 w-full bg-white"
-        content={
-          <div className="-mt-5 flex grow flex-col items-center justify-center px-5 text-center">
-            <Image src={welcomeTree} alt="welcome" width={300} height={150} />
-            <p className="mb-3 mt-8 text-mainTitle2-bold">
-              환영해요 {data?.user?.name}님
-            </p>
-            <p className="text-subTitle2-medium text-text-sub-gray4f">
-              친구에게 내 소개를 부탁해보세요
-              <br />
-              친구가 소개서를 작성해주면
-              <br />내 정원에 나무가 심겨요
-            </p>
+      <BaseLayout
+        showHeader={false}
+        className={cn('h-calc-h overflow-y-scroll')}
+      >
+        <div className="-mt-5 flex grow flex-col items-center justify-center px-5 text-center">
+          <Image src={welcomeTree} alt="welcome" width={300} height={150} />
+          <p className="mb-3 mt-8 text-t1-kr-b">
+            환영해요 {data?.user?.name}님
+          </p>
+          <p className="text-t3-kr-m text-text-sub-gray4f">
+            친구에게 나에 대해 물어보세요
+            <br />
+            친구가 소개서를 작성해주면
+            <br />내 정원에 나무가 심겨요
+          </p>
 
-            <Modal
-              open={copyModalOpen}
-              onOpenChange={setCopyModalOpen}
-              key="copyLinkModal"
-              title="링크가 복사되었어요"
-              footer={{
-                item: [
-                  <Button
-                    onClick={() => setCopyModalOpen(false)}
-                    variant="BG-accent"
-                    className="border-t-[1px]"
-                    key="copy-close"
-                  >
-                    확인
-                  </Button>,
-                ],
-              }}
-              trigger={<></>}
-            />
+          <Modal
+            open={copyModalOpen}
+            onOpenChange={setCopyModalOpen}
+            key="copyLinkModal"
+            title="링크가 복사되었어요"
+            footer={{
+              item: [
+                <Button
+                  onClick={() => setCopyModalOpen(false)}
+                  variant="BG-accent"
+                  className="border-t-[1px]"
+                  key="copy-close"
+                >
+                  확인
+                </Button>,
+              ],
+            }}
+            trigger={<></>}
+          />
+          <div className="absolute bottom-4 mb-10 mt-auto flex w-full justify-center pb-4">
+            <Button
+              onClick={() => setBottomSheetOpen(true)}
+              variant="BG-brand"
+              className="w-full max-w-xs"
+            >
+              나에 대해 물어보기
+            </Button>
           </div>
-        }
-        button={
-          <Button onClick={() => setBottomSheetOpen(true)} variant="BG-brand">
-            나에 대해 물어보기
-          </Button>
-        }
-      />
+        </div>
+      </BaseLayout>
       <BottomSheetButton
         bottomSheetOpen={bottomSheetOpen}
         closeBottomSheet={closeBottomSheet}
